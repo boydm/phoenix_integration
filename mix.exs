ae6fbe7585b1a1e5ae716b602a0ee509d10bcdae
Boyd Multerer <boyd@multerer.com>
2018-12-18 13:17:59 +1300

    continue fixing up travis

--- mix.exs
+++ mix.exs
@@ -49,6 +49,7 @@ defmodule PhoenixIntegration.Mixfile do
       # Docs dependencies
       {:ex_doc, ">= 0.0.0", only: [:dev, :docs]},
       {:inch_ex, ">= 0.0.0", only: :docs}
+      # {:credo, "~> 0.8.10", only: [:dev, :test], runtime: false}
     ]
   end
 
