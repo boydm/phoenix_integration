bd0c4d1411fdb6b3d4967bfe85fa804e05af579b
Brian Marick <marick@exampler.com>
2020-02-07 14:26:01 -0600

    Bad name deleted.

--- test/details/user_edit_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule PhoenixIntegration.Details.UserEditTest do
-  use ExUnit.Case, async: true
-  # import PhoenixIntegration.Assertions.Map
-  # import PhoenixIntegration.FormSupport
-  # alias PhoenixIntegration.Form.Tree
-
-end
