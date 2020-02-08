d5b99801491fcb00cf1ecd0e53e735ed4d300ccc
Brian Marick <marick@exampler.com>
2020-02-08 14:32:08 -0600

    Convert a tree into changes

--- lib/phoenix_integration/form/change.ex
+++ lib/phoenix_integration/form/change.ex
@@ -10,4 +10,16 @@ defmodule PhoenixIntegration.Form.Change do
   def to(path, new_value) when is_list(path) do
     %__MODULE__{path: path, value: new_value}
   end
+
+  def changes(tree), do: changes(tree, [])
+
+  def changes(tree, path_prefix) when is_map(tree) do
+    Enum.flat_map(tree, fn {key, value} ->
+      changes(value, [key | path_prefix])
+    end)
+  end
+
+  def changes(value, path_prefix) do
+    [to(Enum.reverse(path_prefix), value)]
+  end
 end
