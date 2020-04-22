b6fde2817ce861696b91fd0fe884b8214ae9aabb
Brian Marick <marick@exampler.com>
2020-04-22 17:06:05 -0500

    A separate function that retains value-less tags

--- lib/phoenix_integration/form/tree_finish.ex
+++ lib/phoenix_integration/form/tree_finish.ex
@@ -6,9 +6,23 @@ defmodule PhoenixIntegration.Form.TreeFinish do
   alias PhoenixIntegration.Form.Tag
 
   def to_action_params(tree) when is_map(tree) do
-    ignore_tag_like_HTTP_does = &(&1)
-    to_params_reducer(tree, ignore_tag_like_HTTP_does)
+    ignore_empty_array_tag_like_HTTP_does = fn acc, _key ->
+      acc
+    end
+    to_params_reducer(tree, ignore_empty_array_tag_like_HTTP_does)
   end
+
+  def no_value_msg,
+    do: {:no_value,
+         "Unless given a value, this param will not be sent to the server."}
+    
+  def to_form_params(tree) when is_map(tree) do
+    retain_empty_array_tag = fn acc, key ->
+      Map.put(acc, key, no_value_msg)
+    end
+    to_params_reducer(tree, retain_empty_array_tag)
+  end
+
 
   defp to_params_reducer(tree, empty_list_handler) when is_map(tree) do
     Enum.reduce(tree, %{}, &(to_params_step &1, &2, empty_list_handler))
@@ -19,7 +33,7 @@ defmodule PhoenixIntegration.Form.TreeFinish do
   defp to_params_step({key, %Tag{} = tag}, acc, empty_list_handler) do
     case {tag.has_list_value, tag.values} do
       {_, []} ->
-        empty_list_handler.(acc)
+        empty_list_handler.(acc, key)
       {true, values} -> 
         Map.put(acc, key, values)
       {false, [value]} ->
